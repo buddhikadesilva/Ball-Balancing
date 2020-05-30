8af23c588781c9da853c7ae214363881ff8da2ad
Buddhika P. De Silva <bevylabs@gmail.com>
2020-05-30 19:22:14 +0530

    added more

---
+++ Assets/Scripts/LoadNext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNext : MonoBehaviour {
+
+    private int nextSceneToLoad;
+    private int levelCount;
+    private int levelNext;
+
+	// Use this for initialization
+	void Start () {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+       // levelNext = SceneManager.GetActiveScene().buildIndex + 1;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D collision) {
+      //  SceneManager.LoadScene(nextSceneToLoad);
+        SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame() {
+        SceneManager.LoadScene(2);
+	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+	
+
+}
